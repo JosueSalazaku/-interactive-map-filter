b7c3fe48be1d09f55eb075fba5bda14e1803e3fe
Josué Salazaku <josuesalazaku@gmail.com>
2024-03-12 11:30:41 +0100

    Add method to fetch and display Map Data

--- interactive-map-filter/components/MapCharts.vue
+++ interactive-map-filter/components/MapCharts.vue
@@ -1,21 +1,34 @@
 <template>
-    <div ref="charts" class="">
-
-    </div>
-</template>
-
-<script setup>
-import * as echarts from 'echarts';
-
-export default {
+    <div ref="chart" style="width: 100%; height: 400px;"></div>
+  </template>
+  
+  <script>
+  import * as echarts from 'echarts';
+  
+  export default {
     data() {
-        return {
-            chart: null,
-        };
+      return {
+        chart: null,
+      };
     },
     mounted() {
-        this.chart = echarts.init(this.$refs.chart)
+      this.chart = echarts.init(this.$refs.chart);
+      this.fetchAndRenderMapData();
     },
-}
-</script>
-
+    methods: {
+      async fetchAndRenderMapData() {
+        const geoJSONData = await this.fetchGeoJSONData();
+        this.renderMap(geoJSONData);
+      },
+      async fetchGeoJSONData() {
+        // Use axios or fetch to get GeoJSON data
+        // Return the data
+      },
+      renderMap(geoJSONData) {
+        // Use ECharts API to configure and render the map
+        // Refer to ECharts documentation for map options
+      },
+    },
+  };
+  </script>
+  
